eba70931c5ddd95ecd4544cc673dc97f7593d621
Markiian Kostiv <markiian.kostiv@gmail.com>
2024-08-27 14:29:44 +0300

    Change congifs settings

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,13 +1,17 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+const localhost = process.env.LOCAL_HOST;
+const deployHost = process.env.DEPLOY_HOST;
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [`${localhost}`, `${deployHost}`],
     methods: ["GET", "POST"],
   },
 });
